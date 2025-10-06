e0cf2720c322dbeddcb1a42bcec253dfd71207c9
awwpotato <awwpotato@voidq.com>
2025-10-06 15:49:45 -0700

    add homepage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,7 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Chaotic Chickens</title>
 </svelte:head>
 
 {@render children?.()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-col justify-center min-h-svh gap-8 p-8">
+	<h1 class="text-center text-3xl font-bold">Chaotic Chickens :3</h1>
+	<button class="text-l rounded bg-slate-900 px-4 py-2 text-center disabled:text-white/50">
+		Log In
+	</button>
+</div>
