003503105a2ca3f86104ddb6bdb91ea8b3a45edc
Hanya You <youh@catlin.edu>
2025-10-23 17:18:55 -0700

    feat: colored borders depending on status of teams in match

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -4,22 +4,25 @@
         "Cooper",
     ])
 
-    let current_match: Array<{team: number, color: string}> = [
-        { team: 1540, color: "red" },
-        { team: 2910, color: "red" },
-        { team: 1323, color: "red" },
-        { team: 254, color: "blue" },
-        { team: 1844, color: "blue" },
-        { team: 1678, color: "blue" },
+    /*  Status: 0: not sent out - 1: sent but not recieved - 2: recieved
+        Subject to change???
+    */
+    let current_match: Array<{team: number, color: string, status: number}> = [
+        { team: 1540, color: "red", status: 0 },
+        { team: 2910, color: "red", status: 2 },
+        { team: 1323, color: "red", status: 1 },
+        { team: 254, color: "blue", status: 2 },
+        { team: 1844, color: "blue", status: 0},
+        { team: 1678, color: "blue", status: 1 },
     ]
 
-    let next_match: Array<{team: number, color: string}> = [
-        { team: 5940, color: "red" },
-        { team: 694, color: "red" },
-        { team: 1690, color: "red" },
-        { team: 9450, color: "blue" },
-        { team: 2811, color: "blue" },
-        { team: 3636, color: "blue" },
+    let next_match: Array<{team: number, color: string, status: number}> = [
+        { team: 5940, color: "red", status: 2 },
+        { team: 694, color: "red", status: 1},
+        { team: 1690, color: "red", status: 1 },
+        { team: 9450, color: "blue", status: 0 },
+        { team: 2811, color: "blue", status: 2 },
+        { team: 3636, color: "blue", status: 0 },
     ]
 
 
@@ -30,13 +33,13 @@
     
 </script>
 
-<div class="grid grid-cols-3 gap-3 m-auto mt-3 w-6xl">
+<div class="grid lg:grid-cols-3 grid-cols-1 gap-3 m-auto mt-3 w-sm lg:w-6xl">
     <div class="col-span-1 grid grid-rows-2 gap-3">
         <div class="row-span-1 bg-slate-900 p-3 rounded">
             Current Match
             <div class="grid grid-cols-3 gap-2 grid-rows-2 rounded mt-2">
-                {#each current_match as {team, color} }
-                    <div class="col-span-1 row-span-1 bg-slate-950 p-2 rounded {color == "red" ? "text-red-500" : "text-blue-500"}">
+                {#each current_match as {team, color, status} }
+                    <div class="col-span-1 row-span-1 bg-slate-950 p-2 rounded {color == "red" ? "text-red-500" : "text-blue-500"} border {status == 0 ? "border-none" : "border-solid"} border-2 {status == 1 ? "border-orange-400" : "border-green-400"}">
                         {team}
                     </div>
                 {/each}
@@ -45,8 +48,8 @@
         <div class="row-span-1 bg-slate-900 p-3 rounded">
             Next Match
             <div class="grid grid-cols-3 gap-2 grid-rows-2 rounded mt-2">
-                {#each next_match as {team, color} }
-                    <div class="col-span-1 row-span-1 bg-slate-950 p-2 rounded {color == "red" ? "text-red-500" : "text-blue-500"}">
+                {#each next_match as {team, color, status} }
+                    <div class="col-span-1 row-span-1 bg-slate-950 p-2 rounded {color == "red" ? "text-red-500" : "text-blue-500"} border {status == 0 ? "border-none" : "border-solid"} border-2 {status == 1 ? "border-orange-400" : "border-green-400"}">
                         {team}
                     </div>
                 {/each}
@@ -57,8 +60,8 @@
         Queue
         {#if scout_queue.length > 0}
             {#each scout_queue as scout, i}
-                <div class="bg-slate-950 rounded mt-2 p-2 grid grid-cols-12">
-                    <button class="bg-slate-800 rounded col-span-1" onclick={() => remove_scout(i)}>❎</button>
+                <div class="bg-slate-950 rounded mt-2 p-2 grid grid-cols-12 items-center">
+                    <button class="bg-slate-800 rounded col-span-1 py-1" onclick={() => remove_scout(i)}>×</button>
                     <span class="col-span-11">
                         {scout}
                     </span>
