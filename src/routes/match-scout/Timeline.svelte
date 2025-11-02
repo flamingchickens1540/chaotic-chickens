5a6e021f1e6fad5548b0f57b45abb3d1979bae2b
0xda157 <da157@voidq.com>
2025-11-02 14:15:02 -0800

    match-scout: add header, footer, drawer

---
+++ src/routes/match-scout/Timeline.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Drawer from '$lib/components/Drawer.svelte';
+
+	let {
+		displaying = $bindable()
+	}: {
+		displaying: boolean;
+	} = $props();
+</script>
+
+<Drawer bind:displaying></Drawer>
