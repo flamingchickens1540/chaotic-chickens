895fcdbc082af982c61cce13b88a54e3ea815440
0xda157 <da157@voidq.com>
2025-11-02 13:42:38 -0800

    add brand colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex flex-col justify-center min-h-svh gap-8 p-8">
 	<h1 class="text-center text-3xl font-bold">Chaotic Chickens :3</h1>
-	<button class="text-l rounded bg-slate-900 px-4 py-2 text-center disabled:text-white/50">
+	<button class="text-l rounded bg-gunmetal px-4 py-2 text-center disabled:text-white/50">
 		Log In
 	</button>
 </div>
