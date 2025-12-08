32596d13fc0de96ebe083f3bd5a9992261c2b8eb
0xda157 <da157@voidq.com>
2025-12-07 21:06:05 -0800

    actually disable buttons

--- src/routes/match-scout/Header.svelte
+++ src/routes/match-scout/Header.svelte
@@ -14,10 +14,6 @@
 		prev?: () => void;
 		next?: () => void;
 	} = $props();
-
-	const disabled = 'pointer-events-none opacity-30';
-	const can_next = $derived(game_stage === 'Post' ? disabled : '');
-	const can_prev = $derived(game_stage === 'Auto' ? disabled : '');
 </script>
 
 <header class="flex justify-between gap-1 border-b-1 border-white/10 p-2 text-xl font-bold">
@@ -26,10 +22,10 @@
 	</span>
 	<div class="align-item-center flex gap-2">
 		<span class="select-none">{game_stage}</span>
-		<button onclick={() => prev()} class={can_prev}>
+		<button onclick={() => prev()} disabled={game_stage === 'Auto'} class="disabled:opacity-30">
 			<ArrowLeft />
 		</button>
-		<button onclick={() => next()} class={can_next}>
+		<button onclick={() => next()} disabled={game_stage === 'Post'} class="disabled:opacity-30">
 			<ArrowRight />
 		</button>
 	</div>
--- src/routes/match-scout/Timeline.svelte
+++ src/routes/match-scout/Timeline.svelte
@@ -12,37 +12,19 @@
 		timeline: Timeline;
 	} = $props();
 
-	function remove_auto(index: number) {
-		timeline.auto.splice(index, 1);
-	}
-
-	function remove_tele(index: number) {
-		timeline.tele.splice(index, 1);
-	}
-
-	function moveUp() {
+	const moveUp = () => {
 		const firstTele = timeline.tele.shift();
 		timeline.auto.push(firstTele!);
-	}
-	function moveDown() {
+	};
+	const moveDown = () => {
 		const lastAuto = timeline.auto.pop()!;
 		// NOTE This function will never be called while the conversion is invalid
 		// Thus our conversion is safe
 		timeline.tele.unshift(lastAuto as any);
-	}
+	};
 
-	const auto_len = $derived(timeline.auto.length);
-	const tele_len = $derived(timeline.tele.length);
-
-	const lastAutoAction = $derived(timeline.auto[auto_len - 1]);
-
-	const disabled = 'pointer-events-none opacity-30';
-	const can_move_up: string = $derived(tele_len === 0 ? disabled : '');
-	const can_move_down: string = $derived(
-		auto_len === 0 || lastAutoAction.includes('Intake') || lastAutoAction == 'LeaveStart'
-			? disabled
-			: ''
-	);
+	let auto_len = $derived(timeline.auto.length);
+	let tele_len = $derived(timeline.tele.length);
 </script>
 
 <Drawer bind:displaying>
@@ -51,9 +33,13 @@
 	{/each}
 	{#if auto_len + tele_len > 0}
 		<div class="flex w-full items-center gap-8 rounded bg-gunmetal p-2">
-			<button class={can_move_up} onclick={moveUp}><MoveUp /></button>
+			<button class="disabled:opacity-30" disabled={tele_len === 0} onclick={moveUp}
+				><MoveUp /></button
+			>
 			<hr class="h-1 grow" />
-			<button class={can_move_down} onclick={moveDown}><MoveDown /></button>
+			<button class="disabled:opacity-30" disabled={auto_len === 0} onclick={moveDown}
+				><MoveDown /></button
+			>
 		</div>
 	{/if}
 	{#each timeline.auto as action, i}
