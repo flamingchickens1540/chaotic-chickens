34cd4875ae903d74603a359d10062f5256e344d3
autumnwilkes <autumnawilkes@gmail.com>
2025-12-10 09:02:14 -0800

    fix: clean up queue ts

--- src/routes/queue/+page.svelte
+++ src/routes/queue/+page.svelte
@@ -1,38 +1,33 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
-	import { LocalStore, localStore } from '@/localStore.svelte';
+	import { localStore } from '@/localStore.svelte';
 	import { io, type Socket } from 'socket.io-client';
-	import { onMount } from 'svelte';
 	const username = localStore('username', 'no name');
 	let queue_full = $state(false);
-	let socket: Socket;
+	const socket: Socket = io({ auth: { username: username.value } });
 
-	onMount(() => {
-		socket = io({ auth: { username: username.value } });
-
-		socket.on('connect', () => {
-			socket.emit('join_queue');
-		});
-		socket.on('queue_full', () => {
-			queue_full = true;
-		});
-		socket.on(
-			'recieve_robot',
-			([match_key, { team_key, color }]: [string, { team_key: string; color: 'red' | 'blue' }]) => {
-				browser &&
-					localStorage.setItem('match_data', '') &&
-					localStorage.setItem('match_key', match_key) &&
-					localStorage.setItem('team_key', team_key) &&
-					localStorage.setItem('color', color);
-				goto('/match-scout');
-			}
-		);
-		socket.on('scout_left_queue', (scout: string) => {
-			if (scout === username.value) {
-				goto('/');
-			}
-		});
+	socket.on('connect', () => {
+		socket.emit('join_queue');
+	});
+	socket.on('queue_full', () => {
+		queue_full = true;
+	});
+	socket.on(
+		'recieve_robot',
+		([match_key, { team_key, color }]: [string, { team_key: string; color: 'red' | 'blue' }]) => {
+			browser &&
+				localStorage.setItem('match_data', '') &&
+				localStorage.setItem('match_key', match_key) &&
+				localStorage.setItem('team_key', team_key) &&
+				localStorage.setItem('color', color);
+			goto('/match-scout');
+		}
+	);
+	socket.on('scout_left_queue', (scout: string) => {
+		if (scout === username.value) {
+			goto('/');
+		}
 	});
 </script>
 
