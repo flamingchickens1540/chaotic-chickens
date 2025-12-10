e75aefa9f2d5dbbc165f146ccbb0e459ae18247f
autumnwilkes <autumnawilkes@gmail.com>
2025-12-10 09:02:14 -0800

    fix: clean up queue ts

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,25 +1,10 @@
-import { prisma } from '$lib/prisma';
 import type { PageLoad } from './$types';
-import { localStore } from '@/localStore.svelte';
 import { goto } from '$app/navigation';
 
 export const load: PageLoad = async () => {
-	const id = localStore('scout_id', -1);
-	if (!id.value) {
+	const id = localStorage.getItem('scout_id');
+	const name = localStorage.getItem('username');
+	if (!id || !name) {
 		goto('/login');
 	}
-
-	const user: { id: number; username: string } | null = await prisma.user.findUnique({
-		where: { id: id.value },
-		select: {
-			id: true,
-			username: true
-		}
-	});
-
-	if (!user) {
-		goto('/login');
-	}
-
-	return user?.username;
 };
