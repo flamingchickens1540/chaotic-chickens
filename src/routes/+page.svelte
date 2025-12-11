fc6417728f51ebc8c23ec600ed35bdf56fcb46e0
0xda157 <da157@voidq.com>
2025-12-11 12:49:21 -0800

    fix homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,12 @@
 	import { localStore } from '@/localStore.svelte';
 	import { LogOut } from 'lucide-svelte';
 
-	let { id, username }: PageProps = $props();
+	let username = $state(localStore('username', ''));
+	let scout_id = $state(localStore('scout_id', -1));
 
 	const logout = () => {
-		localStore('username', '').reset();
-		localStore('id', -1).reset();
+		username.reset();
+		scout_id.reset();
 		goto('/login');
 	};
 </script>
@@ -17,7 +18,7 @@
 <div class="flex flex-col gap-4 p-4">
 	<div class="grid grid-rows-3 gap-2">
 		<button onclick={logout}><LogOut /></button>
-		<span>Hello {username}</span>
+		<span>Hello {username.value}</span>
 	</div>
 	<div class="flex flex-col gap-2">
 		<button class="rounded bg-gunmetal p-2 disabled:opacity-30" onclick={() => goto('queue')}
