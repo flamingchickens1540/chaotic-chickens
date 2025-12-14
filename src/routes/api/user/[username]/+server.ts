9f7bdb2ec0ad8d59edd3a8a394714729f83642f8
autumnwilkes <autumnawilkes@gmail.com>
2025-12-14 09:09:05 -0800

    fix: type error on api route

--- src/routes/api/user/[username]/+server.ts
+++ src/routes/api/user/[username]/+server.ts
@@ -1,23 +1,30 @@
 import { prisma } from '$lib/prisma';
-import { json, type RequestEvent, type RequestHandler } from '@sveltejs/kit';
+import { error, json, type RequestEvent, type RequestHandler } from '@sveltejs/kit';
 
 // All of this is very insecure - fine for bunnybots, but for the main season I would like a better implementation
 
 // Create account / Login
-export const POST: RequestHandler = async ({ request, params: { username } }: RequestEvent) => {
+export const POST: RequestHandler = async ({ params: { username } }: RequestEvent) => {
+	if (!username) {
+		return error(400, { message: 'Username must be provided' });
+	}
+
 	const user = await prisma.user.create({ data: { username } });
 	return json(user);
 };
 
 // Login
-export const GET: RequestHandler = async ({ request, params: { username } }: RequestEvent) => {
+export const GET: RequestHandler = async ({ params: { username } }: RequestEvent) => {
+	if (!username) {
+		return error(400, { message: 'Username must be provided' });
+	}
 	const user = await prisma.user.findFirst({ where: { username } });
+
 	return json(user ?? (await prisma.user.create({ data: { username } })));
 };
 // Update name, admin status
 export const PATCH: RequestHandler = async ({ request, params: { username } }: RequestEvent) => {
 	const { id }: { id: number } = await request.json();
-
 	return json(
 		await prisma.user.update({
 			where: { id },
